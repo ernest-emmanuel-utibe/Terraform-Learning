730b7d31783f32fecd8f1f60832ceb2997917f16
ernest-emmanuel-utibe <em123nest@gmail.com>
2024-05-03 12:47:50 +0100

    first commit to the repo

---
+++ main.tf
@@ -0,0 +1,28 @@
+# Learning HCl with 3RN3ST
+
+
+# Block
+# A block is a container for other content
+# Blocks are defined by their type and enclosed in curly braces {}.
+block_type {
+    attribute1 = value1
+    attribute2 = value2
+}
+
+# Here is a terraform coed to create an instance
+resource "aws_instance" "example" {
+    ami = "ami-0c94855ba95c71c99"
+    instance_type = "t2.micro"
+}
+
+# Attributes
+# Attributes are 'key' 'value' and 'pair'
+
+
+# Datatypes
+
+# Conditions 
+
+# Function
+
+# Resource Dependency
